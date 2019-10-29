4d59b230afd5e05f66363bdca451b1e287fddbab
ruinan.li02 <ruinan.li02@ele.me>
2019-10-29 14:58:15 +0800

    feat: 初始化问价结构

--- src/libs/umd-lib.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace umdLib {
-  const version: string
-  function doSomething(): void
-}
-
-// umd库必须加上这一条
-export as namespace umdLib
-
-export = umdLib
--- src/node/b.node.ts
+++
@@ -1,7 +0,0 @@
-// 导出多个变量
-// exports === module.exports
-exports.c = 3
-exports.d = 4
-
-// 会覆盖上面的导出
-// module.exports = {}
