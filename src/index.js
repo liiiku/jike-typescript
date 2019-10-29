5e9fecfd44b6778bf42c138e127bca9f33f9bca8
ruinan.li02 <ruinan.li02@ele.me>
2019-10-29 15:37:47 +0800

    feat: 基础tsconfig的配置和文件相关

---
+++ src/index.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+// import './datatype'
+// import './enum'
+// import './interface'
+// import './function'
+// import './class'
+// import './class1'
+// import './es6/c'
+// import './node/c.node'
+// import './merge/merge'
+require("./libs/index");
+var hello = 'hello Typescript!';
+document.querySelectorAll('.app')[0].innerHTML = hello;
