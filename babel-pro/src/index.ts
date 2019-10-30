5e9d1cbfcaa8feb76704e2d91cd9a84852f9a5c7
ruinan.li02 <ruinan.li02@ele.me>
2019-10-30 16:08:13 +0800

    feat: 简单的ts-laoder 到babel的配置

---
+++ babel-pro/src/index.ts
@@ -0,0 +1,19 @@
+// class A {
+//   a: number = 1
+// }
+
+// let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 }
+
+// let n = { x, y, ...z }
+// // n= 1
+
+// let s = {} as A 
+// s.a = 1
+
+// 在babel编译中会报错
+// const enum E { A }  常量枚举
+// export = s 默认导出
+
+const hello1: string = 'hello Typescript!';
+document.querySelectorAll('.app')[0].innerHTML = hello1
+// hello = 1;
