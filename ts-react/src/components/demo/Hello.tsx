aa83a342b53b2b9eb2ffb1103aa6bc3dca5d49fd
ruinan.li02 <ruinan.li02@ele.me>
2019-10-31 11:59:04 +0800

    feat: 简单搭建ts和react项目结构

---
+++ ts-react/src/components/demo/Hello.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Greeting {
+  name: string
+}
+
+const Hello = (props: Greeting) => {
+  return (
+    <h1>{props.name}</h1>
+  )
+}
+
+export default Hello;
---
+++ ts-react/src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './components/demo/Hello';
+
+ReactDOM.render(
+  <Hello name="Typescript" />,
+  document.querySelectorAll('.app')[0]
+)
